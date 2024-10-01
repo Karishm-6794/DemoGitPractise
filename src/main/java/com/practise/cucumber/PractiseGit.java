72a5c7ff347989f55c645583aae70ca43b313483
mredd <karishmakandula88@gmail.com>
2024-10-01 12:19:57 +0530

    New java file is added

---
+++ src/main/java/com/practise/cucumber/PractiseGit.java
@@ -0,0 +1,21 @@
+package com.practise.cucumber;
+
+import org.junit.Test;
+
+public class PractiseGit {
+	
+	
+	
+	@Test
+	public void student1()
+	{
+		System.out.println("student name is karishma");
+	}
+	
+	@Test
+	public void student2()
+	{
+		System.out.println("student name is shamini");
+	}
+
+}
